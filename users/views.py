7345ca6eba230a831cfbab2a2af45d86c34dede5
Ashok <ashokshar006@gmail.com>
2022-12-25 16:52:40 +0530

    project app base: handles file uploads and text extraction logic

--- users/views.py
+++ users/views.py
@@ -7,31 +7,37 @@ from .forms import UserRegisterForm
 
 def user_signup(request):
     
-    form = UserRegisterForm()
-    if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Account created Successfully.')
-            return redirect('login')
+    if request.user.is_authenticated:
+        return redirect('index')
+    else:
+        form = UserRegisterForm()
+        if request.method == 'POST':
+            form = UserRegisterForm(request.POST)
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'Account created Successfully.')
+                return redirect('login')
 
     return render(request, 'users/signup.html', context={'form': form})
 
 
 def user_login(request):
 
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+    if request.user.is_authenticated:
+        return redirect('index')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
 
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            messages.success(request, 'Logged In successfully.')
-            return redirect('register')
-        else:
-            messages.success(request, 'Invalid credentials.')
-            return redirect('login')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'Logged In successfully.')
+                return redirect('index')
+            else:
+                messages.success(request, 'Invalid credentials.')
+                return redirect('login')
     return render(request, 'users/login.html')
 
 
