88595fcfc3d8f49b983c2c8555c5156ba48bdaaa
Ashok <ashokshar006@gmail.com>
2022-12-25 16:15:19 +0530

    user register view: logic for user register and set up templates

--- users/views.py
+++ users/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
 
 
 
 def user_signup(request):
-    pass
+    
+    form = UserRegisterForm()
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account created Successfully.')
+            return redirect('login')
 
+    return render(request, 'users/signup.html', context={'form': form})
 
 
 def user_login(request):
