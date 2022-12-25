34e56e8ac79899d8db77d84dcbdce984519dd341
Ashok <ashokshar006@gmail.com>
2022-12-25 17:26:41 +0530

    File Upload View: Handles file upload logic

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import UserFile, FileText
 
 # Register your models here.
+
+
+admin.site.register((UserFile, FileText))
--- base/views.py
+++ base/views.py
@@ -1,6 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import UserFile, FileText
 
 
 
 def index(request):
-    return render(request, 'base/index.html')
+    # Filtering all files, uploaded by current user(request.user)
+    user = request.user
+    user_files = user.userfile_set.all().order_by('-uploaded_at')
+    file_count = user_files.count()
+
+    context = {'user_files': user_files, 'file_count': file_count}
+    return render(request, 'base/index.html', context)
+
+
+def upload_file(request):
+
+    if request.method == 'POST':
+        file = request.FILES['doc']
+        user = request.user
+
+        new_file = UserFile.objects.create(
+            user=user,
+            file=file,
+            filename=file
+        )
+        new_file.save()
+        messages.success(request, "File uploaded.")
+        return redirect('index')
+
+    return render(request, 'base/upload_file.html')
