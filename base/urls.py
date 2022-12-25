810acf86a4b9c69233b89ecb0e2697c887c5a12c
Ashok <ashokshar006@gmail.com>
2022-12-25 17:59:46 +0530

    file validation: implement validation to upload only pdf file

--- base/urls.py
+++ base/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('upload/', views.upload_file, name='upload_file'),
+    path('detail/<int:file_id>/', views.file_detail, name='file_detail'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from .models import UserFile, FileText
 
@@ -16,17 +16,38 @@ def index(request):
 
 def upload_file(request):
 
+    # Handles file uploads and create a new file object in database
     if request.method == 'POST':
         file = request.FILES['doc']
         user = request.user
 
-        new_file = UserFile.objects.create(
-            user=user,
-            file=file,
-            filename=file
-        )
-        new_file.save()
-        messages.success(request, "File uploaded.")
-        return redirect('index')
+        file_exc = str(file)
+        if (file_exc.split('.')[-1].lower()) == 'pdf':
+            new_file = UserFile.objects.create(
+                user=user,
+                file=file,
+                filename=file
+            )
+            new_file.save()
+            messages.success(request, "File uploaded.")
+            return redirect('index')
+        else:
+            messages.info(request, 'Please select \'pdf\' file')
+            return redirect('upload_file')
+
+    # Handles text extraction from uploaded file and save in text database model
+
+    """Code goes here"""
 
     return render(request, 'base/upload_file.html')
+
+
+def file_detail(request, file_id):
+
+    file_obj = get_object_or_404(UserFile, id=file_id)
+    file = file_obj.file
+    filename = file_obj.filename
+    uploaded = file_obj.uploaded_at
+
+    context = {'file': file, 'filename': filename, 'uploaded': uploaded}
+    return render(request, 'base/file_detail.html', context)
