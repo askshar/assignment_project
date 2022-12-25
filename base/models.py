6e7123c504fa144d5fe7a93248f4010670d15700
Ashok <ashokshar006@gmail.com>
2022-12-25 17:06:37 +0530

    Models: database models in base app

--- base/models.py
+++ base/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class UserFile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    file = models.FileField(upload_to='user_uploads/')
+    filename = models.CharField(max_length=255, null=True, blank=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        if not self.filename:
+            self.filename='default.pdf'
+            return self.filename
+        return self.filename
+
+
+class FileText(models.Model):
+    file = models.OneToOneField(UserFile, on_delete=models.CASCADE)
+    filename = models.CharField(max_length=255, null=True, blank=True)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.filename
